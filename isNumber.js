60ad04225287e32b708ee723293d9810e304d5ff
JRG Pulido <jrgpulido@gmail.com>
2019-02-28 19:24:32 -0600

    Add files via upload

---
+++ isNumber.js
@@ -0,0 +1,12 @@
+//isNumber(x)
+//-> x
+//<- true si es número
+//<- false si no es número
+
+var isNumber = function (x) {
+//function isNumber(x) {
+  if (typeof x === 'number')
+    return true;
+  else
+    return false;
+}
